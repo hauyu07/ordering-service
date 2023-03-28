ab321adb105d5d330143023f60ea9358f9c84a98
hauyu07 <c.hauyu07@gmail.com>
2023-03-28 10:37:16 +0800

    Refactor/controller-methods
    commit f5da216dba3c5c9a0ec855c4cf21a862c2b454f4
    Author: hauyu07 <c.hauyu07@gmail.com>
    Date:   Tue Mar 28 10:36:43 2023 +0800
    
        refactor: moved menu POST handler to restaurant controller

--- src/main/java/io/hauyu07/orderingservice/controller/MenuController.java
+++ src/main/java/io/hauyu07/orderingservice/controller/MenuController.java
@@ -1,19 +1,17 @@
 package io.hauyu07.orderingservice.controller;
 
-import io.hauyu07.orderingservice.dto.MenuCreationDto;
 import io.hauyu07.orderingservice.dto.MenuDto;
 import io.hauyu07.orderingservice.entity.Menu;
-import io.hauyu07.orderingservice.entity.MenuCategory;
-import io.hauyu07.orderingservice.entity.MenuItem;
 import io.hauyu07.orderingservice.mapper.MenuMapper;
 import io.hauyu07.orderingservice.service.MenuCategoryService;
 import io.hauyu07.orderingservice.service.MenuItemService;
 import io.hauyu07.orderingservice.service.MenuService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Menu")
 @RestController
@@ -43,22 +41,6 @@ public class MenuController {
     public MenuDto getMenuById(@PathVariable Long id) {
         Menu menu = menuService.getMenuById(id);
         return menuMapper.menuToMenuDto(menu);
-    }
-
-    // TODO: refactor creation of menu along with the associated nested entities
-    @PostMapping("/restaurants/{restaurantId}/menus")
-    public MenuCreationDto createMenu(@PathVariable Long restaurantId, @RequestBody MenuCreationDto menuCreationDto) {
-        Menu menu = menuMapper.menuDtoToMenu(menuCreationDto);
-        Menu createdMenu = menuService.createMenu(restaurantId, menu);
-        List<MenuCategory> categories = menuMapper.menuCategoryDtoListToMenuCategoryList(menuCreationDto.getCategories());
-        for (MenuCategory category : categories) {
-            MenuCategory createdMenuCategory = menuCategoryService.createMenuCategory(createdMenu.getId(), category);
-            List<MenuItem> items = category.getItems();
-            for (MenuItem item : items) {
-                menuItemService.createMenuItem(createdMenuCategory.getId(), item);
-            }
-        }
-        return menuCreationDto;
     }
 
 //    @PutMapping("/menus/{id}")
--- src/main/java/io/hauyu07/orderingservice/controller/RestaurantController.java
+++ src/main/java/io/hauyu07/orderingservice/controller/RestaurantController.java
@@ -1,13 +1,23 @@
 package io.hauyu07.orderingservice.controller;
 
+import io.hauyu07.orderingservice.dto.MenuCreationDto;
 import io.hauyu07.orderingservice.dto.RestaurantCreationDto;
 import io.hauyu07.orderingservice.dto.RestaurantDto;
+import io.hauyu07.orderingservice.entity.Menu;
+import io.hauyu07.orderingservice.entity.MenuCategory;
+import io.hauyu07.orderingservice.entity.MenuItem;
 import io.hauyu07.orderingservice.entity.Restaurant;
+import io.hauyu07.orderingservice.mapper.MenuMapper;
 import io.hauyu07.orderingservice.mapper.RestaurantMapper;
+import io.hauyu07.orderingservice.service.MenuCategoryService;
+import io.hauyu07.orderingservice.service.MenuItemService;
+import io.hauyu07.orderingservice.service.MenuService;
 import io.hauyu07.orderingservice.service.RestaurantService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Restaurant")
 @RestController
@@ -18,7 +28,19 @@ public class RestaurantController {
     private RestaurantService restaurantService;
 
     @Autowired
+    private MenuService menuService;
+
+    @Autowired
+    private MenuCategoryService menuCategoryService;
+
+    @Autowired
+    private MenuItemService menuItemService;
+
+    @Autowired
     private RestaurantMapper restaurantMapper;
+
+    @Autowired
+    private MenuMapper menuMapper;
 
 //    @GetMapping
 //    public Page<Restaurant> getAllRestaurants(
@@ -38,6 +60,22 @@ public class RestaurantController {
         Restaurant restaurant = restaurantMapper.restaurantCreationDtoToRestaurant(restaurantCreationDto);
         restaurantService.createRestaurant(restaurant);
         return restaurantCreationDto;
+    }
+
+    // TODO: refactor creation of menu along with the associated nested entities
+    @PostMapping("/{id}/menus")
+    public MenuCreationDto createMenu(@PathVariable Long id, @RequestBody MenuCreationDto menuCreationDto) {
+        Menu menu = menuMapper.menuDtoToMenu(menuCreationDto);
+        Menu createdMenu = menuService.createMenu(id, menu);
+        List<MenuCategory> categories = menuMapper.menuCategoryDtoListToMenuCategoryList(menuCreationDto.getCategories());
+        for (MenuCategory category : categories) {
+            MenuCategory createdMenuCategory = menuCategoryService.createMenuCategory(createdMenu.getId(), category);
+            List<MenuItem> items = category.getItems();
+            for (MenuItem item : items) {
+                menuItemService.createMenuItem(createdMenuCategory.getId(), item);
+            }
+        }
+        return menuCreationDto;
     }
 
 //    @PutMapping("/{id}")
