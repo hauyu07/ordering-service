a041a1f6af4cdf15d5726ea335c18574f323492c
hauyu07 <c.hauyu07@gmail.com>
2023-04-01 22:54:29 +0800

    enhance: ignore unmapped

--- src/main/java/io/hauyu07/orderingservice/mapper/RestaurantMapper.java
+++ src/main/java/io/hauyu07/orderingservice/mapper/RestaurantMapper.java
@@ -5,8 +5,9 @@ import io.hauyu07.orderingservice.dto.RestaurantDto;
 import io.hauyu07.orderingservice.entity.Restaurant;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RestaurantMapper {
 
     @Mapping(target = "menus", ignore = true)
