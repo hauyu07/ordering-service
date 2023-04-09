02db0b604765596a5044201f6650e4640524e028
hauyu07 <c.hauyu07@gmail.com>
2023-04-09 22:45:48 +0800

    Enhance/restaurant-order
    commit 6b0ab8696d33955a3a48f07096aca939175f795e
    Author: hauyu07 <c.hauyu07@gmail.com>
    Date:   Sun Apr 9 22:45:25 2023 +0800
    
        enhance: delete order
    
    commit 7f788907aae6b76292bc0413267008738d722779
    Author: hauyu07 <c.hauyu07@gmail.com>
    Date:   Sun Apr 9 22:41:14 2023 +0800
    
        enhance: added summary
    
    commit 6bc45e417b3d89861f5ceb0514e91225cd87175d
    Author: hauyu07 <c.hauyu07@gmail.com>
    Date:   Sun Apr 9 19:47:55 2023 +0800
    
        enhance: remove endpoint
    
    commit b624d441e42da11fae5d714bbcbdba0b81605e91
    Author: hauyu07 <c.hauyu07@gmail.com>
    Date:   Sun Apr 9 19:45:37 2023 +0800
    
        enhance: order creation
    
    commit 33050faa9898feadabaee40c3fb562e7233bb9f4
    Author: hauyu07 <c.hauyu07@gmail.com>
    Date:   Sun Apr 9 19:05:47 2023 +0800
    
        enhance: list out orders for a restaurant

--- src/main/java/io/hauyu07/orderingservice/controller/OrderController.java
+++ src/main/java/io/hauyu07/orderingservice/controller/OrderController.java
@@ -1,20 +1,20 @@
 package io.hauyu07.orderingservice.controller;
 
+import io.hauyu07.orderingservice.dto.OrderCreationDto;
 import io.hauyu07.orderingservice.dto.OrderDto;
-import io.hauyu07.orderingservice.dto.OrderItemCreationDto;
-import io.hauyu07.orderingservice.entity.MenuItem;
+import io.hauyu07.orderingservice.dto.OrderListingDto;
 import io.hauyu07.orderingservice.entity.Order;
-import io.hauyu07.orderingservice.entity.OrderItem;
 import io.hauyu07.orderingservice.mapper.OrderMapper;
 import io.hauyu07.orderingservice.service.MenuItemService;
-import io.hauyu07.orderingservice.service.OrderItemService;
 import io.hauyu07.orderingservice.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Tag(name = "Order")
 @RestController
@@ -25,27 +25,39 @@ public class OrderController {
     private OrderService orderService;
 
     @Autowired
-    private OrderItemService orderItemService;
-
-    @Autowired
     private MenuItemService menuItemService;
 
     @Autowired
     private OrderMapper orderMapper;
 
+    @GetMapping
+    @Operation(summary = "List orders of the current user's restaurant")
+    public List<OrderListingDto> getRestaurantOrders(Principal principal) {
+        List<Order> orders = orderService.getOrdersByRestaurantUser(principal.getName());
+        return orderMapper.orderListToOrderListingDtoList(orders);
+    }
+
     @GetMapping("/{id}")
+    @Operation(summary = "Get details of a specific order")
     public OrderDto getOrderById(@PathVariable Long id) {
         Order order = orderService.getOrderById(id);
         return orderMapper.orderToOrderDto(order);
     }
 
-    @PostMapping("/{id}/items")
-    public OrderDto appendOrderItem(@PathVariable Long id, @RequestBody List<OrderItemCreationDto> orderItemCreationDtos) {
-        List<OrderItem> orderItems = orderItemCreationDtos.stream().map(item -> {
-            MenuItem menuItem = menuItemService.getMenuItemById(item.getMenuItemId());
-            return new OrderItem(menuItem, item.getQuantity());
-        }).collect(Collectors.toList());
-        Order order = orderItemService.createMultipleOrderItems(id, orderItems);
-        return orderMapper.orderToOrderDto(order);
+    @PostMapping
+    @Operation(summary = "Create an order for the current user's restaurant (scenario of a waiter taking order )")
+    public ResponseEntity<String> createRestaurantOrder(
+            Principal principal,
+            @RequestBody OrderCreationDto orderCreationDto
+    ) {
+        orderService.createOrderByRestaurantUser(principal.getName(), orderCreationDto);
+        return ResponseEntity.ok("Success");
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a specific order")
+    public ResponseEntity<String> deleteRestaurantOrder(@PathVariable Long id) {
+        orderService.deleteOrderById(id);
+        return ResponseEntity.ok("Success");
     }
 }
--- src/main/java/io/hauyu07/orderingservice/service/OrderService.java
+++ src/main/java/io/hauyu07/orderingservice/service/OrderService.java
@@ -1,14 +1,20 @@
 package io.hauyu07.orderingservice.service;
 
+import io.hauyu07.orderingservice.dto.OrderCreationDto;
+import io.hauyu07.orderingservice.dto.OrderItemCreationDto;
+import io.hauyu07.orderingservice.entity.MenuItem;
 import io.hauyu07.orderingservice.entity.Order;
-import io.hauyu07.orderingservice.entity.Restaurant;
+import io.hauyu07.orderingservice.entity.OrderItem;
+import io.hauyu07.orderingservice.entity.User;
 import io.hauyu07.orderingservice.exception.ResourceNotFoundException;
 import io.hauyu07.orderingservice.mapper.OrderMapper;
-import io.hauyu07.orderingservice.repository.OrderItemRepository;
-import io.hauyu07.orderingservice.repository.OrderRepository;
-import io.hauyu07.orderingservice.repository.RestaurantRepository;
+import io.hauyu07.orderingservice.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -23,7 +29,20 @@ public class OrderService {
     private RestaurantRepository restaurantRepository;
 
     @Autowired
+    private MenuItemRepository menuItemRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private OrderMapper orderMapper;
+
+    public List<Order> getOrdersByRestaurantUser(String userId) {
+        User user = userRepository
+                .findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
+        return user.getRestaurant().getOrders();
+    }
 
     public Order getOrderById(Long orderId) {
         return orderRepository
@@ -31,11 +50,33 @@ public class OrderService {
                 .orElseThrow(() -> new ResourceNotFoundException("Order", "id", orderId));
     }
 
-    public Order createOrder(Long restaurantId, Order order) {
-        Restaurant restaurant = restaurantRepository
-                .findById(restaurantId)
-                .orElseThrow(() -> new ResourceNotFoundException("Restaurant", "id", restaurantId));
-        order.setRestaurant(restaurant);
-        return orderRepository.save(order);
+    @Transactional
+    public void createOrderByRestaurantUser(String userId, OrderCreationDto orderCreationDto) {
+        User user = userRepository
+                .findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
+
+        List<OrderItemCreationDto> orderItemCreationDtos = orderCreationDto.getItems();
+        Order order = orderMapper.orderCreationDtoToOrder(orderCreationDto);
+        order.setRestaurant(user.getRestaurant());
+        Order createdOrder = orderRepository.save(order);
+
+        List<OrderItem> orderItemsToCreate = new ArrayList<>();
+        for (OrderItemCreationDto orderItemCreationDto : orderItemCreationDtos) {
+            OrderItem orderItem = new OrderItem();
+            Long menuItemId = orderItemCreationDto.getMenuItemId();
+            MenuItem menuItem = menuItemRepository
+                    .findById(menuItemId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Menu Item", "id", menuItemId));
+            orderItem.setMenuItem(menuItem);
+            orderItem.setOrder(createdOrder);
+            orderItem.setQuantity(orderItemCreationDto.getQuantity());
+            orderItemsToCreate.add(orderItem);
+        }
+        orderItemRepository.saveAll(orderItemsToCreate);
+    }
+
+    public void deleteOrderById(Long id) {
+        orderRepository.deleteById(id);
     }
 }
