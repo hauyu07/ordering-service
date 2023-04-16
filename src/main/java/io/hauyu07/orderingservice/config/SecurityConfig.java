062767c740b91a01c79840b492cf8e635e3a564c
hauyu07 <c.hauyu07@gmail.com>
2023-04-16 19:23:32 +0800

    Fix/cors

--- src/main/java/io/hauyu07/orderingservice/config/SecurityConfig.java
+++ src/main/java/io/hauyu07/orderingservice/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://restaurantpos.hauyu07.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
